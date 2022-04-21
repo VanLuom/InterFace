0196900912541a6809f838707d2880fa78073038
VanLuom <lluom14@gmail.com>
2022-04-21 08:49:54 +0700

    first commit

--- src/advance/dev/MainApp.java
+++ src/advance/dev/MainApp.java
@@ -1,4 +1,4 @@
-package advance.dev1;
+package advance.dev;
 import java.util.Scanner;
 
 import advance.dev.dao.Ishape;
@@ -18,8 +18,11 @@ public class MainApp {
 		for (int i = 0; i < 2; i++) {
 			System.out.println("nhap ban kinh");
 			double bk = sc.nextDouble();
+			Circle cr = new Circle(bk);
 			sc.nextLine();
 			shapemanger.add(new Circle(bk));
+			cr.chuVi();
+			cr.dienTich();
 			
 		System.out.println("nhap a");
 		double a = sc.nextDouble();
@@ -28,10 +31,16 @@ public class MainApp {
 		double b = sc .nextDouble();
 		sc.nextLine();
 		shapemanger.add(new Rectangle(a, b ));
+		Rectangle re = new Rectangle(a, b);
+		re.chuVi();
+		re.dienTich();
+		
 		
 			
 		}
+		
 		System.out.println("nhap vao  1 hinh tam giac");
+		Triangle tr = new Triangle(10,13,15);
 		System.out.println("nhap a");
 		double a = sc.nextDouble();
 		sc.nextLine();
@@ -41,7 +50,8 @@ public class MainApp {
 		System.out.println("nhap c");
 		double c = sc.nextDouble();
 		sc.nextLine();
-		
+		tr.chuVi();
+		tr.dienTich();
 		shapemanger.add(new Triangle(a, b, c ));
 	}
 
